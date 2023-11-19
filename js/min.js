83c60b798a70e383d7e515049f0d6e36038be977
mahmoud <146541832+mahmoud-hachem2024@users.noreply.github.com>
2023-11-19 14:12:36 +0100

    Update min.js
    recuperation les valeurs de champs

--- js/min.js
+++ js/min.js
@@ -1,1 +1,11 @@
+//Récupérer les valeurs des champs du formulaire
 const fullName=document.querySelector('.fullName');
+const dateOfBirth=document.querySelector('.dateOfBirth');
+const gender=document.querySelector('select[name="gender"]');
+const task1=document.querySelector('.task1');
+const task2=document.querySelector('.task2');
+const task3=document.querySelector('.task3');
+//Récupérer les bouttons
+const result =document.querySelector('.result');
+const submit =document.querySelector('.submit');
+const reset =document.querySelector('.reset');
